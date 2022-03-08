04661b11a1222948c0d27f6a3abf63b6d232ee5b
Julio-Vasquez <jualvalitube@gmail.com>
2022-03-07 23:30:19 -0500

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './pages/App'
+import './sass/_main.scss'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
---
+++ src/pages/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@mantine/core'
+
+function App() {
+  return (
+    <div className="App">
+      <Button color="green">btn</Button>
+    </div>
+  )
+}
+
+export default App
